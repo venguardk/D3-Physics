11bb28035a21f68bb72d38412691b99801221622
venguardk <venguardk@users.noreply.github.com>
2023-05-20 02:27:40 -0700

    added hole

--- game.js
+++ game.js
@@ -9,48 +9,65 @@ class Test extends Phaser.Scene{
         this.load.path = "./assets/";
         this.load.image('ball', 'Ball Sprite.png');
         this.load.image('grass','Ground Tile.png');
+        this.load.image('flag', 'Flag Sprite.png');
     }
 
     create(){
-        // implementing putting
         const graphics = this.add.graphics({lineStyle: {width: 10, color: 0xffb600, alpha: 0.5}});
         const line = new Phaser.Geom.Line();
-        const putt = this.add.image(100, 100, 'ball');
-        const tee = this.add.image(100, 200, 'grass');
+        // const putt = this.add.image(100, 100, 'ball');
+        // const tee = this.add.image(100, 200, 'grass');
 
         // golf ball implementation
-        const golf = this.physics.add.image(putt.x, putt.y, 'ball');
+        const golf = this.physics.add.image(100, 120, 'ball');
+        golf.setDepth(1);
         golf.setScale(0.5);
         golf.setBounce(0.5, 0.5);
         golf.setDrag(0.5, 0.5);
         golf.setCircle(50);
         golf.setCollideWorldBounds(true);
         golf.setDamping(true);
-
         
-        
-        golf.disableBody(true, true);
+        //implementing putting
+        // golf.disableBody(true, true);
 
         let angle = 0;
 
         this.input.on('pointermove', (pointer) =>{
-            angle = Phaser.Math.Angle.BetweenPoints(tee, pointer);
-            putt.rotation = angle;
-            Phaser.Geom.Line.SetToAngle(line, tee.x, tee.y - 100, angle, 128);
+            angle = Phaser.Math.Angle.BetweenPoints(golf, pointer);
+            // putt.rotation = angle;
+            Phaser.Geom.Line.SetToAngle(line, golf.x, golf.y, angle, 128);
             graphics.clear().strokeLineShape(line);
         });
 
         this.input.on('pointerup', () =>{
-            golf.enableBody(true, tee.x, tee.y - 100, true, true);
-            this.physics.velocityFromRotation(angle, 600, golf.body.velocity);
+            // golf.enableBody(true, tee.x, tee.y - 100, true, true);
+            if (golf.body.velocityx == undefined && golf.body.velocityy == undefined){
+                this.physics.velocityFromRotation(angle, 600, golf.body.velocity);
+            }
+            
         });
 
         //platform implementation
-        const platform = this.add.tileSprite(400, 300, 100 * 10, 100, 'grass');
+        const platform = this.add.tileSprite(400, 200, 100 * 15, 100, 'grass');
         this.physics.add.existing(platform, true);
+        const ground = this.add.tileSprite(1500, 1020, 100 * 5, 100, 'grass');
+        this.physics.add.existing(ground, true);
+
+        //implementing hole
+        const goal = this.add.image(1800, 970, 'flag');
+        const zone = this.add.zone(goal.x, goal.y, 100, 100);
+        this.physics.add.existing(zone, true);
+        this.physics.add.overlap(zone, golf, (_zone, golf) =>{
+            golf.setAlpha(0.5);
+        })
 
         //colliders
         this.physics.add.collider(golf, platform);
+        this.physics.add.collider(golf, ground);
+        
+
+        this.add.text(10, 10, `ball velocity: ${golf.body.velocityx}`);
     }
 }
 
